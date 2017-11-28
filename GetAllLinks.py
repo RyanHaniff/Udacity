8dbdafbf5515f9c90d1b9b17a2ee3ca01b3a9325
RyanH <haniffryan.hr@gmail.com>
2017-11-28 18:54:55 -0500

    Store the links on the page as a list

--- GetAllLinks.py
+++ GetAllLinks.py
@@ -10,11 +10,13 @@ def get_next_taget(page):
     url = page[start_quote + 1:end_quote]
     return url, end_quote
 
-def print_all_links(page):
+def get_all_links(page):
+    links = []
     while True:
         url, endpos = get_next_taget(page)
         if url:
-            print url
+            links.append(url)
             page = page[endpos:]
         else:
             break
+        return links
